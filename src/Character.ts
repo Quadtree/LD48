89dae73977b27b03a090ceb4ef16dc9f54da08ff
Quadtree <quadtree@gmail.com>
2021-03-01 22:21:36 -0800

    Changes merged in from LD47

--- src/Character.ts
+++ src/Character.ts
@@ -1,16 +1,16 @@
-import { AdvancedDynamicTexture } from "@babylonjs/gui/2D/advancedDynamicTexture";
-import { AmmoJSPlugin } from "@babylonjs/core/Physics/Plugins/ammoJSPlugin";
-import { EventState } from "@babylonjs/core/Misc/observable";
-import { FreeCamera } from "@babylonjs/core/Cameras/freeCamera";
-import { KeyboardEventTypes } from "@babylonjs/core/Events/keyboardEvents";
-import { KeyboardInfo } from "@babylonjs/core/Events/keyboardEvents";
-import { PointerEventTypes } from "@babylonjs/core/Events/pointerEvents";
-import { PointerInfo } from "@babylonjs/core/Events/pointerEvents";
-import { Scene } from "@babylonjs/core/scene";
-import { TextBlock } from "@babylonjs/gui/2D/controls/textBlock";
-import { Vector3 } from "@babylonjs/core/Maths/math.vector";
-import { Actor } from "./am/Actor";
-import { Keys } from "./Keys";
+import {AdvancedDynamicTexture} from "@babylonjs/gui/2D/advancedDynamicTexture";
+import {AmmoJSPlugin} from "@babylonjs/core/Physics/Plugins/ammoJSPlugin";
+import {EventState} from "@babylonjs/core/Misc/observable";
+import {FreeCamera} from "@babylonjs/core/Cameras/freeCamera";
+import {KeyboardEventTypes, KeyboardInfo} from "@babylonjs/core/Events/keyboardEvents";
+import {PointerEventTypes, PointerInfo} from "@babylonjs/core/Events/pointerEvents";
+import {Scene} from "@babylonjs/core/scene";
+import {TextBlock} from "@babylonjs/gui/2D/controls/textBlock";
+import {Vector3} from "@babylonjs/core/Maths/math.vector";
+import {Actor} from "../am/Actor";
+import {Keys} from "../Keys";
+import {CardConsoleColor} from "./CardConsole";
+import {Util} from "../Util";
 
 declare var Ammo:any;
 
@@ -55,26 +55,50 @@ export class Character extends Actor
     private isAdded = true;
     private addCharge = 1;
 
-    public constructor(private scene:Scene, private canvas:HTMLCanvasElement|null){
+    private ghostObject:any;
+
+    private acceptingInput = true;
+
+    public get pos(){
+        let v3 = this.character.getGhostObject().getWorldTransform().getOrigin();
+
+        return this.toBJVector3(v3);
+    }
+
+    public set pos(v:Vector3){
+        this.character.getGhostObject().getWorldTransform().setOrigin(this.toBLVector3(v));
+    }
+
+    public constructor(protected scene:Scene, private canvas:HTMLCanvasElement|null, position:Vector3){
         super()
 
         this.camera = new FreeCamera('', new Vector3(), this.scene);
+        this.camera.minZ = 0.1;
+        this.camera.maxZ = 800;
+        this.camera.inertia = 0;
+        this.camera.angularSensibility = 400;
+        console.log(`fov=${this.camera.fov}`);
         if (this.canvas){
             this.camera.attachControl(this.canvas, false);
         }
+
+        this.camera.keysDown = [];
+        this.camera.keysLeft = [];
+        this.camera.keysUp = [];
+        this.camera.keysRight = [];
 
         let ajsp = this.scene.getPhysicsEngine()!.getPhysicsPlugin() as AmmoJSPlugin
 
         let startTransform = new Ammo.btTransform();
         startTransform.setIdentity();
-        //startTransform.setOrigin (btVector3(0.0, 4.0, 0.0));
-        startTransform.setOrigin(new Ammo.btVector3(-8,10,24));
+        startTransform.setOrigin(this.toBLVector3(position));
 
         let m_ghostObject = new Ammo.btPairCachingGhostObject();
+        this.ghostObject = m_ghostObject;
         m_ghostObject.setWorldTransform(startTransform);
         ajsp.world.getBroadphase().getOverlappingPairCache().setInternalGhostPairCallback(new Ammo.btGhostPairCallback());
-        const characterHeight=1.75;
-        const characterWidth =1.75;
+        const characterHeight = 1.75;
+        const characterWidth = 1.00;
         let capsule = new Ammo.btCapsuleShape(characterWidth,characterHeight);
         m_ghostObject.setCollisionShape(capsule);
         m_ghostObject.setCollisionFlags(CF_CHARACTER_OBJECT);
@@ -89,38 +113,62 @@ export class Character extends Actor
         ajsp.world.addCollisionObject(m_ghostObject, CharacterFilter, StaticFilter|DefaultFilter);
         ajsp.world.addAction(m_character);
 
-        this.scene.onKeyboardObservable.add((ed:KeyboardInfo, es:EventState) => {
-            if (ed.type == KeyboardEventTypes.KEYDOWN || ed.type == KeyboardEventTypes.KEYUP){
-                const evt = ed.event as KeyboardEvent;
-                const down = ed.type == KeyboardEventTypes.KEYDOWN;
-                if (evt.keyCode == Keys.W) this.moveForward = down;
-                if (evt.keyCode == Keys.S) this.moveBackward = down;
-                if (evt.keyCode == Keys.A) this.moveLeft = down;
-                if (evt.keyCode == Keys.D) this.moveRight = down;
-                if (evt.keyCode == Keys.Space) this.jump = down;
-            }
-        });
-
-        debugUi = AdvancedDynamicTexture.CreateFullscreenUI('', true, this.scene);
-        debugUiText = new TextBlock('', 'test')
-        debugUi.addControl(debugUiText)
-        debugUiText.left = -600
-        debugUiText.top = -600
-        debugUiText.color = '#ffffff'
-
         if (this.canvas){
+            this.scene.onKeyboardObservable.add((ed:KeyboardInfo, es:EventState) => {
+                if (this.acceptingInput) {
+                    if (ed.type == KeyboardEventTypes.KEYDOWN || ed.type == KeyboardEventTypes.KEYUP) {
+                        const evt = ed.event as KeyboardEvent;
+                        const down = ed.type == KeyboardEventTypes.KEYDOWN;
+                        if (evt.key == 'w' || evt.key == 'ArrowUp') this.moveForward = down;
+                        if (evt.key == 's' || evt.key == 'ArrowDown') this.moveBackward = down;
+                        if (evt.key == 'a' || evt.key == 'ArrowLeft') this.moveLeft = down;
+                        if (evt.key == 'd' || evt.key == 'ArrowRight') this.moveRight = down;
+                        if (evt.key == ' ') this.jump = down;
+                    }
+                }
+            });
+
+            //debugUi = AdvancedDynamicTexture.CreateFullscreenUI('', true, this.scene);
+            debugUiText = new TextBlock('', 'test')
+            //debugUi.addControl(debugUiText)
+            debugUiText.left = -600
+            debugUiText.top = -600
+            debugUiText.color = '#ffffff'
+
             this.scene.onPointerObservable.add((ed:PointerInfo, es:EventState) => {
-                if (ed.type == PointerEventTypes.POINTERTAP){
-                    this.canvas!.requestPointerLock();
+                if (this.acceptingInput) {
+                    if (ed.type == PointerEventTypes.POINTERTAP && ed.event instanceof PointerEvent) {
+                        this.canvas!.requestPointerLock();
+                    }
+                    if (ed.type == PointerEventTypes.POINTERDOWN) {
+                        if (!this.canvas!.hasPointerCapture((ed.event as PointerEvent).pointerId)) {
+                            this.pointerDown();
+                        }
+                    }
+                    if (ed.type == PointerEventTypes.POINTERUP) {
+                        this.pointerUp();
+                    }
                 }
             });
         }
     }
 
-    public update(delta:number){
-        let v3 = this.character.getGhostObject().getWorldTransform().getOrigin();
+    protected pointerDown(){
 
-        this.camera.position = this.toBJVector3(v3);
+    }
+
+    protected pointerUp(){
+
+    }
+
+    protected getCameraOffset():Vector3 {
+        return new Vector3(0,0,0);
+    }
+
+    public update(delta:number){
+        let v3:btVector3 = this.character.getGhostObject().getWorldTransform().getOrigin();
+
+        this.camera.position = this.toBJVector3(v3).add(new Vector3(0, 0.75, 0)).add(this.getCameraOffset());
 
         const walkSpeed = 0.25;
 
@@ -141,7 +189,9 @@ export class Character extends Actor
         this.character.setWalkDirection(blWalkDirection);
 
         if (this.jump){
-            this.character.jump(this.toBLVector3(new Vector3(0, 20, 0)));
+            const v3 = this.toBLVector3(new Vector3(0, 20, 0));
+            this.character.jump(v3);
+            Util.destroyVector(v3);
             this.jump = false;
         }
 
@@ -163,20 +213,40 @@ export class Character extends Actor
             this.isAdded = true;
         }
 
-        debugUiText.text = `pos=${this.formatVector(v3)}\nwalkDirection=${this.formatVector(blWalkDirection, 4)}\nonGround=${this.character.onGround()}\nisAdded=${this.isAdded} addCharge=${this.addCharge.toFixed(1)} shouldBeAdded=${shouldBeAdded}`
+        if (this.canvas)
+            debugUiText.text = `angle=${this.camera.rotation.y}\npos=${this.formatBJVector(this.toBJVector3(v3))}\nwalkDirection=${this.formatVector(blWalkDirection, 4)}\nonGround=${this.character.onGround()}\nisAdded=${this.isAdded} addCharge=${this.addCharge.toFixed(1)} shouldBeAdded=${shouldBeAdded}\n${this.getExtraText()}`
+
+        Util.destroyVector(blWalkDirection);
+    }
+
+    protected getExtraText():string {
+        return "";
+    }
+
+
+    exitingWorld() {
+        super.exitingWorld();
+
+        if (this.isAdded){
+            this.scene.getPhysicsEngine()!.getPhysicsPlugin().world.removeAction(this.character);
+            this.isAdded = false;
+        }
+
+        const ajsp = this.scene.getPhysicsEngine()!.getPhysicsPlugin() as AmmoJSPlugin
+        ajsp.world.removeCollisionObject(this.ghostObject);
     }
 
     private formatVector(v3:btVector3, fixedPlaces:number = 1){
         return `${v3.x().toFixed(fixedPlaces)},${v3.y().toFixed(fixedPlaces)},${v3.z().toFixed(fixedPlaces)}`;
     }
 
+    private formatBJVector(v3:Vector3, fixedPlaces:number = 1){
+        return `${v3.x.toFixed(fixedPlaces)},${v3.y.toFixed(fixedPlaces)},${v3.z.toFixed(fixedPlaces)}`;
+    }
+
     private toBLVector3(v3:Vector3):btVector3
     {
-        const ret = new Ammo.btVector3();
-        ret.setX(v3.x);
-        ret.setY(v3.y);
-        ret.setZ(v3.z);
-        return ret;
+        return Util.toBLVector3(v3);
     }
 
     private toBJVector3(v3:btVector3):Vector3
@@ -184,7 +254,7 @@ export class Character extends Actor
         return new Vector3(
             v3.x(),
             v3.y(),
-            v3.z()
+            -v3.z()
         );
     }
 }
