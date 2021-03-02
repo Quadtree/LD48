8d64ee595071b7ce1caa4b7180999c296f967303
Quadtree <quadtree@gmail.com>
2021-03-01 22:24:15 -0800

    More code from LD47 merged in

--- src/Character.ts
+++ src/Character.ts
@@ -1,18 +1,16 @@
-import {AdvancedDynamicTexture} from "@babylonjs/gui/2D/advancedDynamicTexture";
-import {AmmoJSPlugin} from "@babylonjs/core/Physics/Plugins/ammoJSPlugin";
-import {EventState} from "@babylonjs/core/Misc/observable";
-import {FreeCamera} from "@babylonjs/core/Cameras/freeCamera";
-import {KeyboardEventTypes, KeyboardInfo} from "@babylonjs/core/Events/keyboardEvents";
-import {PointerEventTypes, PointerInfo} from "@babylonjs/core/Events/pointerEvents";
-import {Scene} from "@babylonjs/core/scene";
-import {TextBlock} from "@babylonjs/gui/2D/controls/textBlock";
-import {Vector3} from "@babylonjs/core/Maths/math.vector";
-import {Actor} from "../am/Actor";
-import {Keys} from "../Keys";
-import {CardConsoleColor} from "./CardConsole";
-import {Util} from "../Util";
+import { AdvancedDynamicTexture } from "@babylonjs/gui/2D/advancedDynamicTexture";
+import { AmmoJSPlugin } from "@babylonjs/core/Physics/Plugins/ammoJSPlugin";
+import { EventState } from "@babylonjs/core/Misc/observable";
+import { FreeCamera } from "@babylonjs/core/Cameras/freeCamera";
+import { KeyboardEventTypes, KeyboardInfo } from "@babylonjs/core/Events/keyboardEvents";
+import { PointerEventTypes, PointerInfo } from "@babylonjs/core/Events/pointerEvents";
+import { Scene } from "@babylonjs/core/scene";
+import { TextBlock } from "@babylonjs/gui/2D/controls/textBlock";
+import { Vector3 } from "@babylonjs/core/Maths/math.vector";
+import { Actor } from "./am/Actor";
+import { Util } from "./Util";
 
-declare var Ammo:any;
+declare var Ammo: any;
 
 const CF_CHARACTER_OBJECT = 16;
 
@@ -21,30 +19,29 @@ const StaticFilter = 2;
 const CharacterFilter = 32;
 
 interface btVector3 {
-    length():number;
-    x():number;
-    y():number;
-    z():number;
-    setX(x:number):void;
-    setY(y:number):void;
-    setZ(z:number):void;
-    setValue(x:number, y:number, z:number):void;
-    normalize():void;
-    rotate(wAxis:btVector3, angle:number):btVector3;
-    dot(v:btVector3):number;
-    op_mul(x:number):btVector3;
-    op_add(v:btVector3):btVector3;
-    op_sub(v:btVector3):btVector3;
+    length(): number;
+    x(): number;
+    y(): number;
+    z(): number;
+    setX(x: number): void;
+    setY(y: number): void;
+    setZ(z: number): void;
+    setValue(x: number, y: number, z: number): void;
+    normalize(): void;
+    rotate(wAxis: btVector3, angle: number): btVector3;
+    dot(v: btVector3): number;
+    op_mul(x: number): btVector3;
+    op_add(v: btVector3): btVector3;
+    op_sub(v: btVector3): btVector3;
 };
 
-let debugUi:AdvancedDynamicTexture
-let debugUiText:TextBlock
+let debugUi: AdvancedDynamicTexture
+let debugUiText: TextBlock
 
-export class Character extends Actor
-{
-    private character:any;
+export class Character extends Actor {
+    private character: any;
 
-    private camera:FreeCamera;
+    private camera: FreeCamera;
 
     private moveForward = false;
     private moveBackward = false;
@@ -55,21 +52,21 @@ export class Character extends Actor
     private isAdded = true;
     private addCharge = 1;
 
-    private ghostObject:any;
+    private ghostObject: any;
 
     private acceptingInput = true;
 
-    public get pos(){
+    public get pos() {
         let v3 = this.character.getGhostObject().getWorldTransform().getOrigin();
 
         return this.toBJVector3(v3);
     }
 
-    public set pos(v:Vector3){
+    public set pos(v: Vector3) {
         this.character.getGhostObject().getWorldTransform().setOrigin(this.toBLVector3(v));
     }
 
-    public constructor(protected scene:Scene, private canvas:HTMLCanvasElement|null, position:Vector3){
+    public constructor(protected scene: Scene, private canvas: HTMLCanvasElement | null, position: Vector3) {
         super()
 
         this.camera = new FreeCamera('', new Vector3(), this.scene);
@@ -78,7 +75,7 @@ export class Character extends Actor
         this.camera.inertia = 0;
         this.camera.angularSensibility = 400;
         console.log(`fov=${this.camera.fov}`);
-        if (this.canvas){
+        if (this.canvas) {
             this.camera.attachControl(this.canvas, false);
         }
 
@@ -99,7 +96,7 @@ export class Character extends Actor
         ajsp.world.getBroadphase().getOverlappingPairCache().setInternalGhostPairCallback(new Ammo.btGhostPairCallback());
         const characterHeight = 1.75;
         const characterWidth = 1.00;
-        let capsule = new Ammo.btCapsuleShape(characterWidth,characterHeight);
+        let capsule = new Ammo.btCapsuleShape(characterWidth, characterHeight);
         m_ghostObject.setCollisionShape(capsule);
         m_ghostObject.setCollisionFlags(CF_CHARACTER_OBJECT);
 
@@ -110,11 +107,11 @@ export class Character extends Actor
 
         console.log(`cc=${m_character}`)
 
-        ajsp.world.addCollisionObject(m_ghostObject, CharacterFilter, StaticFilter|DefaultFilter);
+        ajsp.world.addCollisionObject(m_ghostObject, CharacterFilter, StaticFilter | DefaultFilter);
         ajsp.world.addAction(m_character);
 
-        if (this.canvas){
-            this.scene.onKeyboardObservable.add((ed:KeyboardInfo, es:EventState) => {
+        if (this.canvas) {
+            this.scene.onKeyboardObservable.add((ed: KeyboardInfo, es: EventState) => {
                 if (this.acceptingInput) {
                     if (ed.type == KeyboardEventTypes.KEYDOWN || ed.type == KeyboardEventTypes.KEYUP) {
                         const evt = ed.event as KeyboardEvent;
@@ -135,7 +132,7 @@ export class Character extends Actor
             debugUiText.top = -600
             debugUiText.color = '#ffffff'
 
-            this.scene.onPointerObservable.add((ed:PointerInfo, es:EventState) => {
+            this.scene.onPointerObservable.add((ed: PointerInfo, es: EventState) => {
                 if (this.acceptingInput) {
                     if (ed.type == PointerEventTypes.POINTERTAP && ed.event instanceof PointerEvent) {
                         this.canvas!.requestPointerLock();
@@ -153,20 +150,20 @@ export class Character extends Actor
         }
     }
 
-    protected pointerDown(){
+    protected pointerDown() {
 
     }
 
-    protected pointerUp(){
+    protected pointerUp() {
 
     }
 
-    protected getCameraOffset():Vector3 {
-        return new Vector3(0,0,0);
+    protected getCameraOffset(): Vector3 {
+        return new Vector3(0, 0, 0);
     }
 
-    public update(delta:number){
-        let v3:btVector3 = this.character.getGhostObject().getWorldTransform().getOrigin();
+    public update(delta: number) {
+        let v3: btVector3 = this.character.getGhostObject().getWorldTransform().getOrigin();
 
         this.camera.position = this.toBJVector3(v3).add(new Vector3(0, 0.75, 0)).add(this.getCameraOffset());
 
@@ -177,18 +174,18 @@ export class Character extends Actor
         let forwardVector = new Vector3(Math.sin(angle), 0, Math.cos(angle));
         let rightVector = new Vector3(Math.sin(angle + (Math.PI / 2)), 0, Math.cos(angle + (Math.PI / 2)));
 
-        let walkDirection = new Vector3(0,0,0);
+        let walkDirection = new Vector3(0, 0, 0);
 
         if (this.moveForward) walkDirection.addInPlace(forwardVector.scale(walkSpeed));
         if (this.moveBackward) walkDirection.addInPlace(forwardVector.scale(-walkSpeed));
         if (this.moveRight) walkDirection.addInPlace(rightVector.scale(walkSpeed));
         if (this.moveLeft) walkDirection.addInPlace(rightVector.scale(-walkSpeed));
 
-        let blWalkDirection:btVector3 = this.toBLVector3(walkDirection);
+        let blWalkDirection: btVector3 = this.toBLVector3(walkDirection);
 
         this.character.setWalkDirection(blWalkDirection);
 
-        if (this.jump){
+        if (this.jump) {
             const v3 = this.toBLVector3(new Vector3(0, 20, 0));
             this.character.jump(v3);
             Util.destroyVector(v3);
@@ -197,18 +194,18 @@ export class Character extends Actor
 
         const shouldBeAdded = blWalkDirection.length() > 0.01 || !this.character.onGround();
 
-        if (!shouldBeAdded){
+        if (!shouldBeAdded) {
             this.addCharge -= delta * 5;
         } else {
             this.addCharge = 1;
         }
 
-        if (this.addCharge <= 0 && this.isAdded){
+        if (this.addCharge <= 0 && this.isAdded) {
             this.scene.getPhysicsEngine()!.getPhysicsPlugin().world.removeAction(this.character);
             this.isAdded = false;
         }
 
-        if (this.addCharge >= 1 && !this.isAdded){
+        if (this.addCharge >= 1 && !this.isAdded) {
             this.scene.getPhysicsEngine()!.getPhysicsPlugin().world.addAction(this.character);
             this.isAdded = true;
         }
@@ -219,7 +216,7 @@ export class Character extends Actor
         Util.destroyVector(blWalkDirection);
     }
 
-    protected getExtraText():string {
+    protected getExtraText(): string {
         return "";
     }
 
@@ -227,7 +224,7 @@ export class Character extends Actor
     exitingWorld() {
         super.exitingWorld();
 
-        if (this.isAdded){
+        if (this.isAdded) {
             this.scene.getPhysicsEngine()!.getPhysicsPlugin().world.removeAction(this.character);
             this.isAdded = false;
         }
@@ -236,21 +233,19 @@ export class Character extends Actor
         ajsp.world.removeCollisionObject(this.ghostObject);
     }
 
-    private formatVector(v3:btVector3, fixedPlaces:number = 1){
+    private formatVector(v3: btVector3, fixedPlaces: number = 1) {
         return `${v3.x().toFixed(fixedPlaces)},${v3.y().toFixed(fixedPlaces)},${v3.z().toFixed(fixedPlaces)}`;
     }
 
-    private formatBJVector(v3:Vector3, fixedPlaces:number = 1){
+    private formatBJVector(v3: Vector3, fixedPlaces: number = 1) {
         return `${v3.x.toFixed(fixedPlaces)},${v3.y.toFixed(fixedPlaces)},${v3.z.toFixed(fixedPlaces)}`;
     }
 
-    private toBLVector3(v3:Vector3):btVector3
-    {
+    private toBLVector3(v3: Vector3): btVector3 {
         return Util.toBLVector3(v3);
     }
 
-    private toBJVector3(v3:btVector3):Vector3
-    {
+    private toBJVector3(v3: btVector3): Vector3 {
         return new Vector3(
             v3.x(),
             v3.y(),
--- src/Util.ts
+++ src/Util.ts
@@ -1,17 +1,39 @@
-export class Util
-{
-    static deepEquals(val1:any, val2:any):boolean {
-        if (typeof(val1) != typeof(val2)) return false;
+import { Scene, Sound } from "@babylonjs/core";
+import { AmmoJSPlugin } from "@babylonjs/core/Physics/Plugins/ammoJSPlugin";
+import { Vector3 } from "@babylonjs/core/Maths/math.vector";
+
+declare var Ammo: any;
+
+interface btVector3 {
+    length(): number;
+    x(): number;
+    y(): number;
+    z(): number;
+    setX(x: number): void;
+    setY(y: number): void;
+    setZ(z: number): void;
+    setValue(x: number, y: number, z: number): void;
+    normalize(): void;
+    rotate(wAxis: btVector3, angle: number): btVector3;
+    dot(v: btVector3): number;
+    op_mul(x: number): btVector3;
+    op_add(v: btVector3): btVector3;
+    op_sub(v: btVector3): btVector3;
+};
+
+export class Util {
+    static deepEquals(val1: any, val2: any): boolean {
+        if (typeof (val1) != typeof (val2)) return false;
 
         if (val1 === val2) return true;
         if ((val1 === null) != (val2 === null)) return false;
 
-        if (typeof(val1) == "object"){
-            let keys:any = {};
+        if (typeof (val1) == "object") {
+            let keys: any = {};
             for (let k in val1) keys[k] = true;
             for (let k in val2) keys[k] = true;
 
-            for (let k in keys){
+            for (let k in keys) {
                 if (!Util.deepEquals(val1[k], val2[k])) return false;
             }
             return true;
@@ -20,7 +42,81 @@ export class Util
         return false;
     }
 
-    static deepJsonCopy(val:any):any {
+    static deepJsonCopy(val: any): any {
         return JSON.parse(JSON.stringify(val));
     }
+
+    private static formatVector(v3: btVector3, fixedPlaces: number = 1) {
+        return `${v3.x().toFixed(fixedPlaces)},${v3.y().toFixed(fixedPlaces)},${v3.z().toFixed(fixedPlaces)}`;
+    }
+
+    public static toBLVector3(v3: Vector3): btVector3 {
+        const ret = new Ammo.btVector3();
+        ret.setX(v3.x);
+        ret.setY(v3.y);
+        ret.setZ(-v3.z);
+
+        Util.totalVectors++;
+
+        if (Util.totalVectors > 10000) {
+            console.log(`POSSIBLE MEMORY LEAK ${Util.totalVectors}`);
+        }
+
+        return ret;
+    }
+
+    private static totalVectors = 0;
+
+    public static toBJVector3(v3: btVector3): Vector3 {
+        return new Vector3(
+            v3.x(),
+            v3.y(),
+            -v3.z()
+        );
+    }
+
+    public static destroyVector(v3: btVector3) {
+        Util.totalVectors--;
+        Ammo.destroy(v3);
+    }
+
+    static rayTest(scene: Scene, from: Vector3, to: Vector3): boolean {
+        let ajsp = scene.getPhysicsEngine()!.getPhysicsPlugin() as AmmoJSPlugin
+
+        const world = ajsp.world;
+
+        let fromBTV = null;
+        let toBTV = null;
+        let cb = null;
+        let ret = null;
+
+        try {
+            cb = new Ammo.ClosestRayResultCallback();
+            fromBTV = Util.toBLVector3(from);
+            toBTV = Util.toBLVector3(to);
+
+            ret = false;
+
+            world.rayTest(fromBTV, toBTV, cb);
+
+            ret = cb.m_collisionObject.ptr != 0;
+
+        } finally {
+            if (cb) Ammo.destroy(cb);
+            if (fromBTV) this.destroyVector(fromBTV);
+            if (toBTV) this.destroyVector(toBTV);
+        }
+
+        return ret;
+    }
+
+    static loadSound(url: string, scene: Scene, loop: boolean = false): Promise<Sound> {
+        return new Promise<Sound>((resolve, reject) => {
+            const sound = new Sound("", url, scene, () => {
+                resolve(sound);
+            }, { loop: loop, autoplay: false });
+        });
+    }
+
+    static CHEATS_ENABLED = false;
 }
