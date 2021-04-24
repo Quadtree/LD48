39681342017b33280ffeefbc7296005b755c2e4f
Quadtree <quadtree@gmail.com>
2021-04-24 16:28:18 -0700

    Updated

--- src/actors/Objective.ts
+++ src/actors/Objective.ts
@@ -8,27 +8,36 @@ export class Objective extends Actor {
 
         const playerShips = this.actorManager!.actors.filter(it => it instanceof PlayerShip);
 
+        let zone = 0;
+        this.actorManager!.scene!.fogMode = Scene.FOGMODE_NONE;
+        this.actorManager!.scene!.fogColor.set(0.2, 0.2, 0.2);
+
         if (playerShips.length > 0) {
             const playerShip = playerShips[0] as PlayerShip;
 
             const dist = playerShip.model!.position.length();
 
             if (dist > 1900) {
-                this.actorManager!.scene!.fogMode = Scene.FOGMODE_NONE;
-                this.actorManager!.scene!.fogColor.set(0.2, 0.2, 0.2);
+                // pass
             } else if (dist > 1300){
                 this.actorManager!.scene!.fogMode = Scene.FOGMODE_LINEAR;
                 this.actorManager!.scene!.fogColor.set(0.2, 0.2, 0.2);
+                zone = 1;
             } else if (dist > 700){
                 this.actorManager!.scene!.fogMode = Scene.FOGMODE_LINEAR;
                 this.actorManager!.scene!.fogColor.set(0.3, 0.3, 0.0);
+                zone = 2;
             } else if (dist > 300){
                 this.actorManager!.scene!.fogMode = Scene.FOGMODE_LINEAR;
                 this.actorManager!.scene!.fogColor.set(0.4, 0.0, 0.0);
+                zone = 3;
             } else {
                 this.actorManager!.scene!.fogMode = Scene.FOGMODE_LINEAR;
                 this.actorManager!.scene!.fogColor.set(0.4, 0.0, 0.4);
+                zone = 4;
             }
         }
+
+
     }
 }
