f14345d9a55f8eb099262620590c0f8ea882193b
Quadtree <quadtree@gmail.com>
2021-04-24 20:27:45 -0700

    Updated

--- src/actors/Objective.ts
+++ src/actors/Objective.ts
@@ -3,7 +3,10 @@ import {PlayerShip} from "./PlayerShip";
 import {Scene} from "@babylonjs/core/scene";
 import {Asteroid} from "./Asteroid";
 import {Util} from "../util/Util";
-import {Quaternion, Vector3} from "@babylonjs/core/Maths/math.vector";
+import {Vector3} from "@babylonjs/core/Maths/math.vector";
+
+const TYPE_ASTEROID = 1;
+const TYPE_SQUIDTHING = 2;
 
 export class Objective extends Actor {
     update(delta: number) {
@@ -40,30 +43,32 @@ export class Objective extends Actor {
                 zone = 4;
             }
 
-            let targetAsteroids = 0;
+            const targetOfType:{[key:number]:number} = {};
 
             if (zone == 1){
-                // there should always be asteroids nearby
-                targetAsteroids = 15;
+                targetOfType[TYPE_ASTEROID] = 25;
+                targetOfType[TYPE_SQUIDTHING] = 1;
             }
 
-            const allAsteroids = this.actorManager!.actors.filter(it => it instanceof Asteroid).map(it => it as Asteroid);
+            for (const type in targetOfType) {
+                const allAsteroids = this.actorManager!.actors.filter(it => it instanceof Asteroid).map(it => it as Asteroid);
 
-            let astCount = 0;
+                let astCount = 0;
 
-            for (const ast of allAsteroids){
-                if (ast.mesh!.position.subtract(playerShip.model!.position).length() > 60){
-                    ast.alive = false;
-                } else {
+                for (const ast of allAsteroids) {
+                    if (ast.mesh!.position.subtract(playerShip.model!.position).length() > 60) {
+                        ast.alive = false;
+                    } else {
+                        astCount++;
+                    }
+                }
+
+                while (astCount < targetAsteroids) {
+                    playerShip.model!.rotationQuaternion!.toRotationMatrix(Util.mat);
+
+                    this.actorManager!.add(new Asteroid(Util.randomPointOnSphere(playerShip.model!.position.add(Vector3.TransformCoordinates(new Vector3(0, 0, 30), Util.mat)), 20)));
                     astCount++;
                 }
-            }
-
-            while (astCount < targetAsteroids){
-                playerShip.model!.rotationQuaternion!.toRotationMatrix(Util.mat);
-
-                this.actorManager!.add(new Asteroid(Util.randomPointOnSphere(playerShip.model!.position.add(Vector3.TransformCoordinates(new Vector3(0,0,30), Util.mat)), 20)));
-                astCount++;
             }
         }
 
