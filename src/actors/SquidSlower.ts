30208071357643e577b900af02d571cfc9176b5a
Quadtree <quadtree@gmail.com>
2021-04-24 21:36:43 -0700

    Updated

--- src/actors/SquidSlower.ts
+++ src/actors/SquidSlower.ts
@@ -5,15 +5,13 @@ import {SpawnableTypes} from "./Spawnable";
 import {Color3, Material, Mesh, StandardMaterial} from "@babylonjs/core/index";
 import {Asteroid} from "./Asteroid";
 import {Scene} from "@babylonjs/core";
+import {MeshBuilder} from "@babylonjs/core/Meshes/meshBuilder";
+import {Util} from "../util/Util";
 
 export class SquidSlower extends SquidThing {
     static eyeMat:Material|null = null;
 
-    enteringWorld() {
-        super.enteringWorld();
-
-
-    }
+    private eyeBeam:Mesh|null = null;
 
     enteringView(scene: Scene) {
         super.enteringView(scene);
@@ -30,10 +28,31 @@ export class SquidSlower extends SquidThing {
                 mesh.material = SquidSlower.eyeMat;
             }
         }
+
+        this.eyeBeam = MeshBuilder.CreateCylinder("", {});
+        this.eyeBeam.material = SquidSlower.eyeMat;
+        Util.setVisibility(this.eyeBeam, false);
     }
 
     protected fireWeapon(playerShip:PlayerShip){
+        const rangeToTarget = playerShip.model!.position.subtract(this.model!.position).length();
 
+        if (rangeToTarget < 120){
+            Util.setVisibility(this.eyeBeam!, true);
+
+            this.eyeBeam!.position = playerShip.model!.position.add(this.model!.position).scaleInPlace(0.5);
+            this.eyeBeam!.scaling.z = rangeToTarget;
+            this.eyeBeam!.lookAt(playerShip.model!.position);
+
+        } else {
+            Util.setVisibility(this.eyeBeam!, false);
+        }
+    }
+
+    exitingView() {
+        super.exitingView();
+
+        this.eyeBeam!.dispose();
     }
 
     getSpawnableType(): string {
